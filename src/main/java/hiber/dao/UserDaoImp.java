577bac01797e74a2017478238132f293cc823fc4
diesel <kapustinym@mail.ru>
2021-06-30 03:37:12 +0300

    Initial commit

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,38 @@
+package hiber.dao;
+
+
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+   @Override
+   public User getUserByCar(String model, int series) {
+      String HQL="FROM User user LEFT OUTER JOIN FETCH user.car WHERE user.car.model=:model AND user.car.series=:series";
+      List<User> user = sessionFactory.getCurrentSession().createQuery(HQL, User.class).setParameter("model", model).setParameter("series", series).getResultList();
+      if (!user.isEmpty()) {
+         return user.get(0);
+      }
+      return null;
+   }
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,42 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "model")
+    String model;
+    @Column(name = "series")
+    int series;
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    public Car() {
+
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
